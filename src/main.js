20b669a72b2781bd0adff12af469184882f9c373
Ruttiyaporn Kongtrakul <kongtrakulmint@gmail.com>
2023-04-13 16:05:49 +0700

    install and define firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import router from './router'
 
 import './assets/main.css'
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKKxZYbEen-lFxq2ZrXb8urdHxIZDGhmQ",
+    authDomain: "egco427-382606.firebaseapp.com",
+    projectId: "egco427-382606",
+    storageBucket: "egco427-382606.appspot.com",
+    messagingSenderId: "91593605242",
+    appId: "1:91593605242:web:4ebbfe83512cefd0357bc3",
+    measurementId: "G-3P07RFPM07"
+  };
+
+initializeApp(firebaseConfig);
 const app = createApp(App)
 
 app.use(router)
